30fe4d68ce6765bb14893640efaa3498f2f81196
Akihiro-K <kurosaki-akihiro@g.ecc.u-tokyo.ac.jp>
2017-08-07 15:09:08 +0900

    chenged initial packet

--- libraries/nc/shared/shared.cpp
+++ libraries/nc/shared/shared.cpp
@@ -4,7 +4,7 @@ struct FromMarker from_marker = {0, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, 0};
 struct FromGPS from_gps = {{0,0,0},{0,0,0},{0,0,0},{0,0,0},0};
 struct FromLSM from_lsm = {{0,0,0},0};
 
-struct ToFlightCtrl to_fc = {NAV_COMMS_VERSION, 2, 7, {0, 0, 0}, {0, 0, 0}, 1, 0, {0, 0, -0.8}, 0.5, 0, 0.3};
+struct ToFlightCtrl to_fc = {NAV_COMMS_VERSION, 0, 0, {0, 0, 0}, {0, 0, 0}, 1, 0, {0, 0, -0.8}, 0.5, 0, 0.3};
 struct FromFlightCtrl from_fc = {0, 0, 0, {0, 0, 0}, {0, 0, 0}, {0, 0, 0, 0}, 0};
 struct ForDebug for_debug = {{0,0,0,0}, {0,0,0}, {0,0,0}};
 struct ToDronePort to_dp = {0, 0, 0, 0, {0, 0, 0}, {0, 0, 0}, {0, 0, 0, 0}};
