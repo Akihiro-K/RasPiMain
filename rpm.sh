9475c6ae9815039ecdc3cf6d41b01587d87cd8c7
Shota Kuroki <kurokis@users.noreply.github.com>
2017-12-29 23:00:44 +0900

    Added execution shortcut

---
+++ rpm.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# run multimarkerserver in the background and save standard output to stdout.log
+# run raspimain in the foreground
+(cd ../MarkerServer/MultiMarkerServer/build/; ./multimarkerserver > ../output_data/stdout.log 2>&1) & (cd RasPiMain/build; sleep 1s; ./main)
