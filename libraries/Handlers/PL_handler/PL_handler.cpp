2b465967aca3488b2f6edd6cf6fe2b988ab3b759
Shota Kuroki <kurokis@users.noreply.github.com>
2018-01-09 00:24:43 +0900

    Replaced TCP with FIFO in PL_Handler

--- libraries/Handlers/PL_handler/PL_handler.cpp
+++ libraries/Handlers/PL_handler/PL_handler.cpp
@@ -7,28 +7,36 @@ Timer PayloadNotConnected_timer(1); // 1 Hz warning
 
 // =============================================================================
 // Private function declarations:
-void PushbackNewPayloadData(const char * src, size_t len);
+//void PushbackNewPayloadData(const char * src, size_t len);
+void PushbackNewPayloadData(uint8_t component_id, uint8_t message_id, const uint8_t * src, size_t len);
 
 // =============================================================================
 // Public functions:
 
 void PayloadHandler()
 {
-  tcp_client c;
-  c.start_connect(TCP_ADDRESS, TCP_PORT_PAYLOAD);
+  //tcp_client c;
+  //c.start_connect(TCP_ADDRESS, TCP_PORT_PAYLOAD);
+  UTFIFOReader fr = UTFIFOReader("/dev/payload_fifo");
   for(;;)
   {
-    bool connected = c.recv_data(PushbackNewPayloadData);
-    if(PayloadNotConnected_timer.check() && !connected){
-      std::cout << "Payload not connected" << std::endl;
+    //bool connected = c.recv_data(PushbackNewPayloadData);
+    static bool connected = false;
+    connected |= fr.recv_data(PushbackNewPayloadData);
+    if(PayloadNotConnected_timer.check()){
+      if(!connected){
+        std::cout << "No response from payload in last 1 second." << std::endl;
+      }
+      connected = false;
     }
   }
 }
 
 // =============================================================================
 // Private functions:
-void PushbackNewPayloadData(const char * src, size_t len){
-  char temp[CLIENT_BUF_SIZE];
+//void PushbackNewPayloadData(const char * src, size_t len){
+void PushbackNewPayloadData(uint8_t component_id, uint8_t message_id, const uint8_t * src, size_t len){
+  char temp[256];
   memcpy(temp, src, len);
   FromMarker temp_s;
   struct FromMarker * struct_ptr = (struct FromMarker *) temp;
@@ -39,6 +47,6 @@ void PushbackNewPayloadData(const char * src, size_t len){
     temp_s.quaternion[i] = struct_ptr->quaternion[i];
     temp_s.r_var[i] = struct_ptr->r_var[i];
   }
-
+  //std::cout << "position x (payload):" << temp_s.position[0] << std::endl;
   PayloadVector->push_back(temp_s); //put the fresh frame in the vector
 }
